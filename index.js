4a77e91c9d8318297ae3f4b6a666011663820db9
Maurits van Mastrigt <maurits@nerdieworks.nl>
2015-02-19 10:37:42 +0100

    Minor improvements to plugin.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var fs   = require('fs');
 var path = require('path');
 
-module.exports = function math ($, document, cb) {
-	$('head').append(fs.readFileSync(path.join(__dirname, 'assets/math.html'), { encoding: 'UTF-8' }));
+module.exports = function documarkMath ($, document, done) {
+	$('head').append(fs.readFileSync(
+		path.join(__dirname, 'assets/math.html'),
+		{ encoding: 'UTF-8' }
+	));
 
-	cb();
+	done();
 };
