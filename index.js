2042d78b5425bf044c9fc3b23cf5462cc804225f
Maurits van Mastrigt <maurits@nerdieworks.nl>
2015-02-22 00:10:53 +0100

    Inject code in root instead of head.
    Because the head element may not be present.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs   = require('fs');
 var path = require('path');
 
 module.exports = function documarkMath ($, document, done) {
-	$('head').append(fs.readFileSync(
+	$.root().append(fs.readFileSync(
 		path.join(__dirname, 'assets/math.html'),
 		{ encoding: 'UTF-8' }
 	));
