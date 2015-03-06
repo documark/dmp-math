5475781d1c1288ef811e0ef9ab373ac112aa1e00
Maurits van Mastrigt <maurits@nerdieworks.nl>
2015-03-07 00:09:16 +0100

    Append code to head element and bump to v0.1.4.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs   = require('fs');
 var path = require('path');
 
 module.exports = function dmpMath ($, document, done) {
-	$.root().append(fs.readFileSync(
+	$('head').append(fs.readFileSync(
 		path.join(__dirname, 'assets/math.html'),
 		{ encoding: 'UTF-8' }
 	));
